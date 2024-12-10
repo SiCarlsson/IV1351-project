e5bec12d558e4ae6dbd419eb44592e059b592907
Simon Carlsson <sicar@kth.se>
2024-12-11 00:03:47 +0100

    Basic view

--- Seminar4HigherGradeTasks/se/elhbsc/iv1351/source/startup/Main.java
+++ Seminar4HigherGradeTasks/se/elhbsc/iv1351/source/startup/Main.java
@@ -1,5 +1,12 @@
 package Seminar4HigherGradeTasks.se.elhbsc.iv1351.source.startup;
 
+import Seminar4HigherGradeTasks.se.elhbsc.iv1351.source.controller.Controller;
+import Seminar4HigherGradeTasks.se.elhbsc.iv1351.source.view.View;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    Controller contr = new Controller();
+    View view = new View(contr);
+    view.displayMenu();
+  }
 }
--- Seminar4HigherGradeTasks/se/elhbsc/iv1351/source/view/View.java
+++ Seminar4HigherGradeTasks/se/elhbsc/iv1351/source/view/View.java
@@ -1,5 +1,51 @@
 package Seminar4HigherGradeTasks.se.elhbsc.iv1351.source.view;
 
+import java.util.Scanner;
+
+import Seminar4HigherGradeTasks.se.elhbsc.iv1351.source.controller.Controller;
+
 public class View {
-  
+  private Controller contr;
+
+  public View(Controller contr) {
+    this.contr = contr;
+  }
+
+  public void displayMenu() {
+    clearTerminal();
+    Scanner inputScanner = new Scanner(System.in);
+
+    while (true) {
+      String[] defaultAlternatives = { "Log in user", "End program" };
+      System.out.println("Chose an alternative... (integer)\n");
+      for (int i = 0; i < defaultAlternatives.length; i++) {
+        System.out.println((i + 1) + ". " + defaultAlternatives[i]);
+      }
+      String input = inputScanner.nextLine();
+
+      switch (input) {
+        // Log in user
+        case "1":
+          System.out.println("SWITCH CASE 1");
+          break;
+
+        // End program
+        case "2":
+          clearTerminal();
+          inputScanner.close();
+          System.out.println("Thanks for today!\n");
+          System.exit(0);
+          break;
+
+        default:
+          clearTerminal();
+          System.out.println("No need to chose a valid integer number...\n");
+      }
+    }
+  }
+
+  private void clearTerminal() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
 }
